78a231d9902192bca18ad2504ad19a034e1247fd
Andrey Grozs <grozwalker@gmail.com>
2022-02-10 20:51:13 +0000

    set autoenv

--- zsh/settings.zsh
+++ zsh/settings.zsh
@@ -4,6 +4,9 @@
 # Path to your oh-my-zsh installation.
 export ZSH=$HOME/.oh-my-zsh
 
+export AUTOENV_ENV_FILENAME=.autoenv
+export AUTOENV_ENV_LEAVE_FILENAME=.autoenv.leave
+
 # Set name of the theme to load --- if set to "random", it will
 # load a random theme each time oh-my-zsh is loaded, in which case,
 # to know which specific one was loaded, run: echo $RANDOM_THEME
